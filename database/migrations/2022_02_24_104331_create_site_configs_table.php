4ee2075080c5c289d8d92e9edd482c5209b732a0
ucercagatay <cagataygia@gmail.com>
2022-02-25 01:54:49 +0300

    Migrations and Route Groups added

---
+++ database/migrations/2022_02_24_104331_create_site_configs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_configs', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('adress');
+            $table->string('facebook_url');
+            $table->string('');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_configs');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\front\PageController;
+use App\Http\Controllers\back\AddController;
+use App\Http\Controllers\back\DeleteController;
+use App\Http\Controllers\back\UpgradeController;
+use App\Http\Controllers\back\ShowController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +20,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+//Front Routes
+Route::controller(PageController::class)->group(function () {
+    Route::prefix('/page')->group(function () {
+
+    });
+});
+//Back Routes
+Route::controller(AddController::class)->group(function () {
+    Route::prefix('/page')->group(function () {
+
+    });
+});
+//Delete Routes
+
+Route::controller(DeleteController::class)->group(function () {
+    Route::prefix('/page')->group(function () {
+
+    });
+});
+//Upgrade Routes
+
+Route::controller(UpgradeController::class)->group(function () {
+    Route::prefix('/page')->group(function () {
+
+    });
+});
+//Show Routes
+Route::controller(ShowController::class)->group(function () {
+    Route::prefix('/page')->group(function () {
+
+    });
 });
